c0c8a7dfada927ec9d13c2c20d95ea1af2e40d40
sifaoctavia-hub <sifanuroctaviani@gmail.com>
2026-09-18 03:59:09 +0000

    Menambahkan program awal hasil panen

---
+++ panen.py
@@ -0,0 +1,11 @@
+
+print("Sistem Pencatatan Hasil Panen")
+
+hasil_panen = 100
+harga_per_kg = 5000
+
+total = hasil_panen * harga_per_kg
+
+print("Total hasil panen:", hasil_panen, "kg")
+print("Harga per kg:", harga_per_kg)
+print("Total harga:", total)
