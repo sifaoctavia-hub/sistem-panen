0e90c25f40c61caa769183d1b8253597c84236fd
sifaoctavia-hub <sifanuroctaviani@gmail.com>
2026-09-18 04:06:05 +0000

    Menambahkan fitur perhitungan diskon

--- panen.py
+++ panen.py
@@ -6,6 +6,13 @@ harga_per_kg = 5000
 
 total = hasil_panen * harga_per_kg
 
+diskon = 0.10
+potongan = total * diskon
+total_setelah_diskon = total - potongan
+
 print("Total hasil panen:", hasil_panen, "kg")
-print("Harga per kg:", harga_per_kg)
-print("Total harga:", total)
+print("Harga per kg: Rp", harga_per_kg)
+print("Total harga: Rp", total)
+print("Diskon:", diskon * 100, "%")
+print("Potongan harga: Rp", potongan)
+print("Total setelah diskon: Rp", total_setelah_diskon)
